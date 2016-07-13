5c97d26810fb187a7f363965fffcaf95b3042da4
ammgws <ammgws@users.noreply.github.com>
2016-07-13 19:25:54 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,9 @@ config = {
     'description': 'Ammcon server',
     'author': 'ammgws',
     'url': 'https://github.com/ammgws/ammcon',
-    'download_url': 'https://github.com/ammgws/ammcon',
     'author_email': 'ammgws@users.noreply.github.com',
     'version': '1.0',
-    'install_requires': ['nose'],
     'packages': ['NAME'],
-    'scripts': [],
     'name': 'ammcon'
 }
 
