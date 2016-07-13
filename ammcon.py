f9c7a91140f2e4b48221ed2a8ebab829b4ecbdbc
ammgws <ammgws@users.noreply.github.com>
2016-07-13 21:23:09 +0900

    rename to ammcon.py

--- ammcon.py
+++ ammcon.py
@@ -46,60 +46,8 @@ from email.mime.multipart import MIMEMultipart
 from imgurpython import ImgurClient
 import configparser
 
-# Define byte commands used to communicate commands to microcontroller
-# [INIT] [TYPE] [CMD] [END]
-# TYPE: AC = aircon
-#       Bx = lighting
-#       C1 = TV
-#       FF = temp sensor
-
-PCMD_ACoff   = b'\xCE\xAC\x00\x7F'
-PCMD_ACsleep = b'\xCE\xAC\x01\x7F'
-PCMD_ACcheck = b'\xCE\xAC\x02\x7F'
-
-PCMD_temp    = b'\xCE\xFF\xFF\x7F'
-
-PCMD_bedroom_lights_off   = b'\xCE\xB1\x00\x7F'
-PCMD_bedroom_lights_on    = b'\xCE\xB1\x01\x7F'
-PCMD_bedroom_lights_down  = b'\xCE\xB1\x02\x7F'
-PCMD_bedroom_lights_up    = b'\xCE\xB1\x03\x7F'
-PCMD_bedroom_lights_full  = b'\xCE\xB1\x04\x7F'
-PCMD_bedroom_lights_night = b'\xCE\xB1\x05\x7F'
-
-PCMD_myroom_lights_off   = b'\xCE\xB2\x00\x7F'
-PCMD_myroom_lights_on    = b'\xCE\xB2\x01\x7F'
-PCMD_myroom_lights_down  = b'\xCE\xB2\x02\x7F'
-PCMD_myroom_lights_up    = b'\xCE\xB2\x03\x7F'
-PCMD_myroom_lights_full  = b'\xCE\xB2\x04\x7F'
-PCMD_myroom_lights_night = b'\xCE\xB2\x05\x7F'
-
-PCMD_kayoroom_lights_off   = b'\xCE\xB3\x00\x7F'
-PCMD_kayoroom_lights_on    = b'\xCE\xB3\x01\x7F'
-PCMD_kayoroom_lights_down  = b'\xCE\xB3\x02\x7F'
-PCMD_kayoroom_lights_up    = b'\xCE\xB3\x03\x7F'
-PCMD_kayoroom_lights_full  = b'\xCE\xB3\x04\x7F'
-PCMD_kayoroom_lights_night = b'\xCE\xB3\x05\x7F'
-
-PCMD_living_lights_off     = b'\xCE\xB4\x00\x7F'
-PCMD_living_lights_on      = b'\xCE\xB4\x01\x7F'
-PCMD_living_lights_low     = b'\xCE\xB4\x02\x7F'
-PCMD_living_lights_chuukan = b'\xCE\xB4\x03\x7F'
-PCMD_living_lights_yellow  = b'\xCE\xB4\x04\x7F'
-PCMD_living_lights_night   = b'\xCE\xB4\x05\x7F'
-PCMD_living_lights_blue    = b'\xCE\xB4\x06\x7F'
-
-PCMD_living_lights_new_off     = b'\xCE\xB5\x00\x7F'
-PCMD_living_lights_new_on      = b'\xCE\xB5\x01\x7F'
-
-PCMD_living_lights_all_off     = b'\xCE\xB6\x00\x7F'
-PCMD_living_lights_all_on      = b'\xCE\xB6\x01\x7F'
-
-PCMD_tv_pwr    = b'\xCE\xC1\x00\x7F'
-PCMD_tv_mute   = b'\xCE\xC1\x01\x7F'
-PCMD_tv_switch = b'\xCE\xC1\x02\x7F'
+import h_bytecmds
     
-MAX_TEMP_CHANGE = 2.0 # Max realistic temp change allowed in logging interval (default is 1min). (temp sensor is ±0.5degC)
-
 def imgur_upload(imgur_client, image_path):
     '''Upload image to Imgur and return link.'''
     result = imgur_client.upload_from_path(image_path, anon=False)
@@ -846,8 +794,8 @@ class _HangoutsClient(sleekxmpp.ClientXMPP):
         if msg['type'] in ('chat', 'normal'):
             reply = command_parser(msg['from'], msg['body'])
             msg.reply(reply).send()
-                
-if __name__ == '__main__':
+            
+def main():
     # Get absolute path of the dir script is run from
     cwd = sys.path[0]
 	
@@ -920,6 +868,7 @@ if __name__ == '__main__':
     # Start temp logger thread
     # Need to look into using Queue
     # Need to get rid of global var
+    MAX_TEMP_CHANGE = 2.0 # Max realistic temp change allowed in logging interval (default is 1min). (temp sensor is ±0.5degC)
     global prev_temp
     prev_temp = 0
     prev_temp = avr_serial('templog')
@@ -1003,3 +952,6 @@ if __name__ == '__main__':
         print('*****************************Done*******************************')
     else:
         print('Unable to connect to Hangouts.')
+
+if __name__ == '__main__':
+    main()
---
+++ h_bytecmds.py
@@ -0,0 +1,51 @@
+# Define byte commands used to communicate commands to microcontroller
+# [INIT] [TYPE] [CMD] [END]
+# TYPE: AC = aircon
+#       Bx = lighting
+#       C1 = TV
+#       FF = temp sensor
+
+PCMD_ACoff   = b'\xCE\xAC\x00\x7F'
+PCMD_ACsleep = b'\xCE\xAC\x01\x7F'
+PCMD_ACcheck = b'\xCE\xAC\x02\x7F'
+
+PCMD_temp    = b'\xCE\xFF\xFF\x7F'
+
+PCMD_bedroom_lights_off   = b'\xCE\xB1\x00\x7F'
+PCMD_bedroom_lights_on    = b'\xCE\xB1\x01\x7F'
+PCMD_bedroom_lights_down  = b'\xCE\xB1\x02\x7F'
+PCMD_bedroom_lights_up    = b'\xCE\xB1\x03\x7F'
+PCMD_bedroom_lights_full  = b'\xCE\xB1\x04\x7F'
+PCMD_bedroom_lights_night = b'\xCE\xB1\x05\x7F'
+
+PCMD_myroom_lights_off   = b'\xCE\xB2\x00\x7F'
+PCMD_myroom_lights_on    = b'\xCE\xB2\x01\x7F'
+PCMD_myroom_lights_down  = b'\xCE\xB2\x02\x7F'
+PCMD_myroom_lights_up    = b'\xCE\xB2\x03\x7F'
+PCMD_myroom_lights_full  = b'\xCE\xB2\x04\x7F'
+PCMD_myroom_lights_night = b'\xCE\xB2\x05\x7F'
+
+PCMD_kayoroom_lights_off   = b'\xCE\xB3\x00\x7F'
+PCMD_kayoroom_lights_on    = b'\xCE\xB3\x01\x7F'
+PCMD_kayoroom_lights_down  = b'\xCE\xB3\x02\x7F'
+PCMD_kayoroom_lights_up    = b'\xCE\xB3\x03\x7F'
+PCMD_kayoroom_lights_full  = b'\xCE\xB3\x04\x7F'
+PCMD_kayoroom_lights_night = b'\xCE\xB3\x05\x7F'
+
+PCMD_living_lights_off     = b'\xCE\xB4\x00\x7F'
+PCMD_living_lights_on      = b'\xCE\xB4\x01\x7F'
+PCMD_living_lights_low     = b'\xCE\xB4\x02\x7F'
+PCMD_living_lights_chuukan = b'\xCE\xB4\x03\x7F'
+PCMD_living_lights_yellow  = b'\xCE\xB4\x04\x7F'
+PCMD_living_lights_night   = b'\xCE\xB4\x05\x7F'
+PCMD_living_lights_blue    = b'\xCE\xB4\x06\x7F'
+
+PCMD_living_lights_new_off = b'\xCE\xB5\x00\x7F'
+PCMD_living_lights_new_on  = b'\xCE\xB5\x01\x7F'
+
+PCMD_living_lights_all_off = b'\xCE\xB6\x00\x7F'
+PCMD_living_lights_all_on  = b'\xCE\xB6\x01\x7F'
+
+PCMD_tv_pwr    = b'\xCE\xC1\x00\x7F'
+PCMD_tv_mute   = b'\xCE\xC1\x01\x7F'
+PCMD_tv_switch = b'\xCE\xC1\x02\x7F'
