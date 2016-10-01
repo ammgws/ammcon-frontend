8625db8f94366b5b73c1d21ce29170463e967edc
ammgws <ammgws@users.noreply.github.com>
2016-10-02 01:05:36 +0900

    add templates, client-side code, cleanup files

---
+++ static/ammcon_general.js
@@ -0,0 +1,27 @@
+// register click event handler on 'document ready'
+/*
+$(function() {
+    $("a#login").bind('click', function() {
+        $("#loader").show();
+        //alert("Click event detected.");
+        //window.console&&console.log("Click event detected.");
+    });
+});
+*/
+
+$(document).on( "click", ".show-page-loading-msg", function() {
+  var $this = $( this ),
+  theme = $this.jqmData( "theme" ) || $.mobile.loader.prototype.options.theme,
+  /* msgText = $this.jqmData( "msgtext" ) || $.mobile.loader.prototype.options.text,*/
+  msgText = "Logging you into AmmCon",
+  textVisible = $this.jqmData( "textvisible" ) || $.mobile.loader.prototype.options.textVisible,
+  textonly = !!$this.jqmData( "textonly" );
+  html = $this.jqmData( "html" ) || "";
+$.mobile.loading( 'show', {
+  text: msgText,
+  textVisible: textVisible,
+  theme: theme,
+  textonly: textonly,
+  html: html
+  });
+});
