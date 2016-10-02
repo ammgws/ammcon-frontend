51b62920fb4a39ef98ea04113830325781863658
ammgws <ammgws@users.noreply.github.com>
2016-10-02 23:37:30 +0900

    not using celery anymore

--- flask_config.py
+++ flask_config.py
@@ -9,6 +9,3 @@ OAUTH_CREDENTIALS = { 'google': {'id': GOOGLE_LOGIN_CLIENT_ID,
 # List of email addresses allowed to sign in to AmmCon using OAuth
 ALLOWED_EMAILS = ['email1@gmail.com',
                   'email2@gmail.com']
-
-CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
-CELERY_RESULT_BACKEND='amqp://guest:guest@localhost:5672//'
