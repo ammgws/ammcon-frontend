af1a1ddfefe83667fc553d1d595f7df4f9363ede
juaninch <ammgws@users.noreply.github.com>
2017-02-28 19:02:42 +0900

    fix #11 (moment.js)
    Change document.write to jQuery append function to insert moment.js script.

--- ammcon_frontend/momentjs.py
+++ ammcon_frontend/momentjs.py
@@ -2,11 +2,12 @@ from jinja2 import Markup
 
 
 class momentjs(object):
-    def __init__(self, timestamp):
+    def __init__(self, timestamp, attrid):
         self.timestamp = timestamp
+        self.attrid = attrid
 
-    def render(self, format):
-        return Markup("<script>\ndocument.write(moment(\"%s\").%s);\n</script>" % (self.timestamp.strftime("%Y-%m-%dT%H:%M:%S Z"), format))
+    def render(self, fmt):
+        return Markup("<script>\n$(\"#%s\").append(moment(\"%s\").%s);\n</script>" % ( self.attrid, self.timestamp.strftime("%Y-%m-%dT%H:%M:%S Z"), fmt))
 
     def format(self, fmt):
         return self.render("format(\"%s\")" % fmt)
