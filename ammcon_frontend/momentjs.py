dba71f1b2e74421e99188a0f8c3cb020d78f3bcb
juaninch <ammgws@users.noreply.github.com>
2017-03-01 01:03:08 +0900

    use .html rather than append to avoid dates being added multiple times

--- ammcon_frontend/momentjs.py
+++ ammcon_frontend/momentjs.py
@@ -7,7 +7,7 @@ class momentjs(object):
         self.attrid = attrid
 
     def render(self, fmt):
-        return Markup("<script>\n$(\"#%s\").append(moment(\"%s\").%s);\n</script>" % ( self.attrid, self.timestamp.strftime("%Y-%m-%dT%H:%M:%S Z"), fmt))
+        return Markup("<script>\n$(\"#%s\").html(moment(\"%s\").%s);\n</script>" % (self.attrid, self.timestamp.strftime("%Y-%m-%dT%H:%M:%S Z"), fmt))
 
     def format(self, fmt):
         return self.render("format(\"%s\")" % fmt)
