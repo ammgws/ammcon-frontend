d72de5164f6153b63da6f970f9fc6a97a76a1b82
juaninch <ammgws@users.noreply.github.com>
2017-03-04 13:05:58 +0900

    add TODO

--- ammcon_frontend/momentjs.py
+++ ammcon_frontend/momentjs.py
@@ -1,5 +1,6 @@
 from jinja2 import Markup
 
+# TODO: need to figure out how to keep both jquery oriented version and standard JS version
 
 class momentjs(object):
     def __init__(self, timestamp, attrid):
@@ -8,6 +9,9 @@ class momentjs(object):
 
     def render(self, fmt):
         return Markup("<script>\n$(\"#%s\").html(moment(\"%s\").%s);\n</script>" % (self.attrid, self.timestamp.strftime("%Y-%m-%dT%H:%M:%S Z"), fmt))
+
+    def render_std(self, fmt):
+        return Markup("<script>\ndocument.write(moment(\"%s\").%s);\n</script>" % (self.timestamp.strftime("%Y-%m-%dT%H:%M:%S Z"), fmt))
 
     def format(self, fmt):
         return self.render("format(\"%s\")" % fmt)
