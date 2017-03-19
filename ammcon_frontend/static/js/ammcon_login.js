4e4cfdcfc6985f90a0b395d422963c6280ac9448
juaninch <jason@kayoway.com>
2017-03-19 20:28:22 +0900

    update code styling

--- ammcon_frontend/static/js/ammcon_login.js
+++ ammcon_frontend/static/js/ammcon_login.js
@@ -1,12 +1,12 @@
-$(document).on( "click", ".show-page-loading-msg", function() {
-  var $this = $( this ),
-  theme = $this.jqmData( "theme" ) || $.mobile.loader.prototype.options.theme,
-  /* msgText = $this.jqmData( "msgtext" ) || $.mobile.loader.prototype.options.text,*/
+$(document).on("click", ".show-page-loading-msg", function() {
+  var $this = $(this),
+  theme = $this.jqmData("theme") || $.mobile.loader.prototype.options.theme,
+  /* msgText = $this.jqmData("msgtext") || $.mobile.loader.prototype.options.text,*/
   msgText = "Logging you into AmmCon",
-  textVisible = $this.jqmData( "textvisible" ) || $.mobile.loader.prototype.options.textVisible,
-  textonly = !!$this.jqmData( "textonly" );
-  html = $this.jqmData( "html" ) || "";
-$.mobile.loading( 'show', {
+  textVisible = $this.jqmData("textvisible") || $.mobile.loader.prototype.options.textVisible,
+  textonly = !!$this.jqmData("textonly");
+  html = $this.jqmData("html") || "";
+$.mobile.loading('show', {
   text: msgText,
   textVisible: textVisible,
   theme: theme,
